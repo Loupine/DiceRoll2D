ad39da126c371ad0b0ba1b958d0f7e2739475e90
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-30 22:53:19 -0400

    Increase player gravity and jump force

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D
 
 
-const GRAVITY := 0.98
-const JUMP_FORCE := 3
+const GRAVITY := 0.098
+const JUMP_FORCE := 8
 
 var has_touched_floor := true
 var velocity := Vector2.ZERO
 
 
-func _physics_process(_delta : float):
+func _physics_process(delta : float):
+	velocity.y = GRAVITY / delta if is_on_floor() else velocity.y + GRAVITY / delta
+	
 	if Input.is_action_pressed("jump") and has_touched_floor:
-		velocity.y = JUMP_FORCE
+		velocity.y = -JUMP_FORCE / delta
 		has_touched_floor = false
 	if is_on_floor():
 		has_touched_floor = true
