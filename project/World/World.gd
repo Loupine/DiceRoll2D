a95e70da52f716c76d4f2c7c2f13e3651f3e9790
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-31 23:42:19 -0400

    Add score label that updates until the player dies

--- project/World/World.gd
+++ project/World/World.gd
@@ -1,9 +1,19 @@
 extends Control
 
 
+var player_died := false
+var score := 0
+
 onready var ball_obstacle_preload := preload("res://Obstacles/BallObstacle.tscn")
 onready var obstacle_spawn_timer := get_node("ObstacleSpawnTimer")
 onready var restart_button := get_node("RestartButton")
+onready var score_label := get_node("ScoreLabel")
+
+
+func _process(_delta):
+	if player_died == false:
+		score += 1
+		score_label.text = String(score)
 
 
 func _on_ObstacleSpawnTimer_timeout()-> void:
@@ -14,7 +24,8 @@ func _on_ObstacleSpawnTimer_timeout()-> void:
 
 func _on_Player_player_died()-> void:
 	restart_button.rect_position.x = 400
+	player_died = true
 
 
-func _on_RestartButton_pressed():
+func _on_RestartButton_pressed()-> void:
 	var _ignored := get_tree().reload_current_scene()
