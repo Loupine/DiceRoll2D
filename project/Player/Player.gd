613087b47e4d43776a3c476d392b9423335c35a3
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-11-01 00:14:31 -0400

    Update player die() method and gravity logic

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -13,22 +13,23 @@ var velocity := Vector2.ZERO
 onready var playerCollision : CollisionShape2D = get_node("CollisionShape2D")
 
 
-func _physics_process(delta : float):
+func _physics_process(delta : float)-> void:
 	if is_alive:
-		velocity.y = GRAVITY / delta if is_on_floor() else velocity.y + GRAVITY / delta
-		
+		velocity.y = 0.0 if is_on_floor() else velocity.y + GRAVITY / delta
 		if Input.is_action_pressed("jump") and has_touched_floor:
 			velocity.y = -JUMP_FORCE / delta
 			has_touched_floor = false
 		if is_on_floor():
 			has_touched_floor = true
-		
+	else:
+		velocity.y = 0.0 if position.y >= 591 else velocity.y + GRAVITY / delta
+		position.x = 134
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func die()-> void:
 	playerCollision.call_deferred("set_disabled", true)
 	scale.y = .10
-	position.y = 590
 	is_alive = false
 	emit_signal("player_died")
