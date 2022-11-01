b2e74a1f6879e2fa3ea04dcd02766f996d7b3b4a
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-11-01 00:13:08 -0400

    Add main menu and add menu button to world

---
+++ project/MainMenu/MainMenu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_PlayButton_pressed()-> void:
+	var _ignored := get_tree().change_scene("res://World/World.tscn")
--- project/World/World.gd
+++ project/World/World.gd
@@ -7,10 +7,11 @@ var score := 0
 onready var ball_obstacle_preload := preload("res://Obstacles/BallObstacle.tscn")
 onready var obstacle_spawn_timer := get_node("ObstacleSpawnTimer")
 onready var restart_button := get_node("RestartButton")
+onready var menu_button := get_node("MenuButton")
 onready var score_label := get_node("ScoreLabel")
 
 
-func _process(_delta):
+func _process(_delta)-> void:
 	if player_died == false:
 		score += 1
 		score_label.text = String(score)
@@ -24,8 +25,13 @@ func _on_ObstacleSpawnTimer_timeout()-> void:
 
 func _on_Player_player_died()-> void:
 	restart_button.rect_position.x = 400
+	menu_button.rect_position.x = 460
 	player_died = true
 
 
 func _on_RestartButton_pressed()-> void:
 	var _ignored := get_tree().reload_current_scene()
+
+
+func _on_MenuButton_pressed()-> void:
+	var _ignored := get_tree().change_scene("res://MainMenu/MainMenu.tscn")
